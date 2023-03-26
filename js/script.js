2eacd182f52c2ee0577962ef12ee3ab104e0d90f
cunhamariatheresa-ZqrXix <cunha.maria.theresa@gmail.com>
2023-03-25 22:52:38 -0500

    Started defining questions and answers.

--- js/script.js
+++ js/script.js
@@ -1,15 +1,40 @@
 // Variable Definitions
-var startButton = document.querySelector("#start-button")
+var startButton = document.querySelector("#start-button");
+var timer = document.querySelector("timer span");
+var question = document.querySelector("#question");
+var optionsContainer = document.querySelector("#options-container");
 
 // Event Listiners
 startButton.addEventListener("click", startQuiz);
 
+// Index
+var questions = [
+    {
+        question: "Question 1",
+        options: ["option 1", "option 2", "option 3", "option 4"],
+        answer: "answer",
+    }
+    {
+        question: "Question 1",
+        options: ["option 1", "option 2", "option 3", "option 4"],
+        answer: "answer",
+    }
+    {
+        question: "Question 1",
+        options: ["option 1", "option 2", "option 3", "option 4"],
+        answer: "answer",
+    }
+]
+
 // Function Definitions
 function startQuiz() {
-    startButton.style.display="none";
+    question.textContent= questions[currentQuestionIndex].question;
+    optionsContainer.innerHTML= "";
+    for (let option of questions[currentQuestionIndex].options)
+}
+
     showQuestion ();
     startTimer ();
-}
 
 function startTimer() {
     setInterval(function() {
