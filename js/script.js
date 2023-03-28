7d3b6aa5337b49f4e5c62dfaa0db1922ae79522c
cunhamariatheresa-ZqrXix <cunha.maria.theresa@gmail.com>
2023-03-27 22:21:59 -0500

    Added functions for showing next question, starting timer, and selecting answers to quiz questions.

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ var questionText = document.querySelector("#question-text");
 var optionsContainer = document.querySelector("#options-container");
 
 let currentQuestionIndex = 0;
-let timeLeft = 0;
+let timeLeft = 60;
 let TimerInterval;
 
 // Event Listiners
@@ -16,33 +16,43 @@ var questions = [
     {
         question: "Question 1",
         options: ["option 1", "option 2", "option 3", "option 4"],
-        answer: "answer",
+        answer: "option 1",
     },
     {
         question: "Question 1",
         options: ["option 1", "option 2", "option 3", "option 4"],
-        answer: "answer",
+        answer: "option 2",
     },
     {
         question: "Question 1",
         options: ["option 1", "option 2", "option 3", "option 4"],
-        answer: "answer",
+        answer: "option 3",
     }
 ]
 
 // Function Definitions
 function startQuiz() {
+    startTimer();
     questionText.textContent= questions[currentQuestionIndex].question;
     optionsContainer.innerHTML= "";
-    for (let option of questions[currentQuestionIndex].options) 
-    {
-    let option = document.createElement("div");
-    option.classList.add("option");
-    option.textContent= option;
-    option.addEventListener("click", selectOption);
-    optionsContainer.appendChild(option);
+    for (let option of questions[currentQuestionIndex].options) {
+    let optionElement = document.createElement("div");
+    optionElement.classList.add("option");
+    optionElement.textContent= option;
+    optionElement.addEventListener("click", selectOption);
+    optionsContainer.appendChild(optionElement); }
 }
-    startTimer ();
+
+function showNextQuestion() {
+    currentQuestionIndex++;
+    questionText.textContent = questions[currentQuestionIndex].question;
+    optionsContainer.innerHTML= "";
+    for (let option of questions[currentQuestionIndex].options) {
+    let optionElement = document.createElement("div");
+    optionElement.classList.add("option");
+    optionElement.textContent= option;
+    optionElement.addEventListener("click", selectOption);
+    optionsContainer.appendChild(option); }
 }
 
 function startTimer() {
@@ -53,3 +63,23 @@ function startTimer() {
     }, 1000);
 }
 
+function selectOption(event) {
+    let selectedOption = event.target;
+    let selectedAnswer = selectedOption.textContent;
+    let correctAnswer = questions[currentQuestionIndex].answer;
+    if (selectedAnswer === correctAnswer) {
+        score ++;
+        resultContainer.textContent = "Correct!";
+    } else {
+        timeLeft -= 10;
+        if (timeLeft < 0) {
+            timeLeft = 0;
+        }
+        resultContainer.textContent = "Incorrect!";
+        }
+        if (currentQuestionindex< questions.length -1) {
+            setTimeout(showNextQuestion, 1000);
+        } else {
+            endQuiz();
+        }
+        }
